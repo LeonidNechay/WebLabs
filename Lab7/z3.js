b3e2cfdaa1a14db08b7123dec29a0b0738ebe44f
Cumparty <leonidnechay@gmail.com>
2022-05-22 22:45:24 +0300

    Lab7

--- Lab7/z3.js
+++ Lab7/z3.js
@@ -2,12 +2,12 @@ let checkedId = [];
 function Check(id){
     if(!checkedId.includes(id)){
         document.getElementById(id).style.backgroundColor = 'rgb(229,229,229)';
-        document.getElementById('input').value += document.getElementById(id).innerText;
+        // document.getElementById('input') += document.getElementById(id).innerText;
         checkedId.push(id);
     }
     else{
         document.getElementById(id).style.backgroundColor = '';
-        document.getElementById('input').value += "";
-        checkedId = checkedId.filter(id => id != id);
+        // document.getElementById('input').value += "";
+        checkedId = checkedId.splice(checkedId.indexOf(id), 1);
     }
 }
